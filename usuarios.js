c8ef8dacd421db6be8542d309a8d5cd7f16fce2e
pcp-create <pcp@rjserranacompressores.com.br>
2026-07-08 21:43:55 -0300

    Update usuarios.js

--- usuarios.js
+++ usuarios.js
@@ -1,5 +1,20 @@
 const usuarios = [
-  { usuario: "rj", senha: "RJ@2026" },
-  { usuario: "locacao", senha: "Locacao@2026" },
-  { usuario: "admin", senha: "Admin@2026" }
+  {
+    usuario: "admin",
+    senha: "Admin@2026",
+    nome: "Administrador",
+    relatorios: ["equipamentos", "projecao"]
+  },
+  {
+    usuario: "locacao",
+    senha: "Locacao@2026",
+    nome: "Equipe Locação",
+    relatorios: ["equipamentos"]
+  },
+  {
+    usuario: "comercial",
+    senha: "Comercial@2026",
+    nome: "Comercial",
+    relatorios: ["equipamentos", "projecao"]
+  }
 ];
