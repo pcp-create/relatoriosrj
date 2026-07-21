c785012ba1469a56353b5174d8c488782ea1d38d
pcp-create <pcp@rjserranacompressores.com.br>
2026-07-21 18:03:10 -0300

    Update usuarios.js

--- usuarios.js
+++ usuarios.js
@@ -10,62 +10,62 @@ const usuarios = [
     senha: "317622",
     nome: "Guilherme",
     perfil: "Administrador",
-    relatorios: ["equipamentos", "projecao", "programacao", "ordemservico"]
+    relatorios: ["equipamentos", "projecao", "programacao", "ordemservico", "faroloficina"]
   },
   {
     usuario: "jose.lino",
     senha: "Rj@482Km",
     nome: "Jose Lino",
     perfil: "Geral",
-    relatorios: ["equipamentos", "projecao", "programacao", "ordemservico"]
+    relatorios: ["equipamentos", "projecao", "programacao", "ordemservico", "faroloficina"]
   },
   {
     usuario: "alexandro",
     senha: "Lp#731Qa",
     nome: "Alexandro Spaeth",
     perfil: "Geral",
-    relatorios: ["equipamentos", "projecao", "programacao", "ordemservico"]
+    relatorios: ["equipamentos", "projecao", "programacao", "ordemservico", "faroloficina"]
   },
   {
     usuario: "paulo",
     senha: "Mx!954Te",
     nome: "Paulo Duwe",
     perfil: "Geral",
-    relatorios: ["equipamentos", "projecao", "programacao", "ordemservico"]
+    relatorios: ["equipamentos", "projecao", "programacao", "ordemservico", "faroloficina"]
   },
   {
     usuario: "bruno",
     senha: "Vn$286Ru",
     nome: "Bruno Pereira",
     perfil: "Geral",
-    relatorios: ["equipamentos", "projecao", "programacao", "ordemservico"]
+    relatorios: ["equipamentos", "projecao", "programacao", "ordemservico", "faroloficina"]
   },
   {
     usuario: "viviane",
     senha: "Gh*619Pw",
     nome: "Viviane Spaeth",
     perfil: "Geral",
-    relatorios: ["equipamentos", "projecao", "programacao", "ordemservico"]
+    relatorios: ["equipamentos", "projecao", "programacao", "ordemservico", "faroloficina"]
   },
   {
     usuario: "lucas",
     senha: "Qm#482Xv",
     nome: "Lucas Feliciano",
     perfil: "Geral",
-    relatorios: ["equipamentos", "projecao", "programacao", "ordemservico"]
+    relatorios: ["equipamentos", "projecao", "programacao", "ordemservico", "faroloficina"]
   },
   {
     usuario: "ana.paula",
     senha: "Fc@864Zw",
     nome: "Ana Paula",
     perfil: "Geral",
-    relatorios: ["equipamentos", "projecao", "programacao", "ordemservico"]
+    relatorios: ["equipamentos", "projecao", "programacao", "ordemservico", "faroloficina"]
   },
   {
     usuario: "priscyla",
     senha: "Hs%251Nd",
     nome: "Priscyla",
     perfil: "Geral",
-    relatorios: ["equipamentos", "projecao", "programacao", "ordemservico"]
+    relatorios: ["equipamentos", "projecao", "programacao", "ordemservico", "faroloficina"]
   }
 ];
